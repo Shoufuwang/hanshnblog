0be27c636d0614be40cb55714d5cf620eb917de1
wang <wang.shou.fu@gmail.com>
2015-01-21 06:46:29 -0800

    step2

---
+++ hanshinBlog/MaserMenu.h
@@ -0,0 +1,13 @@
+//
+//  MaserMenu.h
+//  hanshinBlog
+//
+//  Created by wang on 2015/1/21.
+//  Copyright (c) 2015年 wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaserMenu : UIViewController
+
+@end
